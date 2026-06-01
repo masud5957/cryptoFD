022f5ef3d4ea12657c22afa2d1bc2fdc25a5afe7
v0agent <it+v0agent@vercel.com>
2026-06-01 14:20:57 +0000

    feat: remove default theme and theme options from NextThemesProvider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -18,7 +18,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   }
 
   return (
-    <NextThemesProvider {...props} attribute="class" suppressHydrationWarning enableSystem storageKey="theme" defaultTheme="dark" themes={['light', 'dark']}>
+    <NextThemesProvider {...props} attribute="class" suppressHydrationWarning enableSystem storageKey="theme">
       {children}
     </NextThemesProvider>
   )
